de07bc96a6d23da2d06f98cd192ee750a61728a9
Rishabh Agrahari <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-03-15 15:44:31 +0530

    added api key

--- twilioproject/settings.py
+++ twilioproject/settings.py
@@ -85,7 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHY_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+AUTHY_KEY = 'zJ5JU9ucu8yQaL1pD9pJGYd32VT7Lk8A'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
